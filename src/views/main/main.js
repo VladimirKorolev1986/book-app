416af6a4d263f7ba1dccf973224503e4bd64de5a
VladimirKorolev <theing1986@gmail.com>
2025-03-08 21:39:04 +0300

    onchange

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,4 +1,5 @@
 import { AbstractView } from "../../common/view.js";
+import onChange from 'on-change'
 
 export class MainView extends AbstractView {
 	state = {
@@ -10,13 +11,22 @@ export class MainView extends AbstractView {
 	constructor(appState){
 		super();
 		this.appState = appState;
+		this.appState = onChange(this.appState, this.appStateHook.bind(this))
 		this.setTitle('Поиск книг');
 	}
 
+	appStateHook(path) {
+		if (path == 'favorites') {
+			// this.render();
+			console.log(path)
+		}
+	}
+
 	render () {
-		const main = document.createElement('div')
-		main.innerHTML = `Число книг: ${this.appState.favorites.length}`
-		this.app.innerHTML = ''
-		this.app.append(main)
+		const main = document.createElement('div');
+		main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
+		this.app.innerHTML = '';
+		this.app.append(main);
+		this.appState.favorites.push('d');
 	}
 }
